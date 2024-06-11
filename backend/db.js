354c433bc892939cb46b374ade43fc430ec2897c
Shlok Jain <85099963+Shlok-Jain@users.noreply.github.com>
2024-06-12 03:00:50 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const monogoURI = 'mongodb+srv://shlokjain0177:QLUgPX8qZwRTAg7f@cluster0.jcw8vbm.mongodb.net/'
+const monogoURI = 'YOUR URI'
 
 const connectToMongo = ()=>{
     mongoose.connect(monogoURI)
